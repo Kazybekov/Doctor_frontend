c87658d79d643464305e25c065ad9ecaeb090c41
Kazybekov <kazbekov.ersin@gmail.com>
2022-11-17 23:35:14 +0600

    not easy

---
+++ src/components/InputTodo.js
@@ -0,0 +1,57 @@
+import React, { Fragment, useState } from "react";
+
+const InputTodo = () => {
+  const [email, setE] = useState("");
+  const [degree, setD] = useState("");
+
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      const body = { email,degree };
+      const response = await fetch("http://localhost:5000/doctor", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const handleChange1 = event => {
+    setE(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
+  const handleChange2 = event => {
+    setD(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">Doctors List</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="email"
+          className="form-control"
+          onChange={handleChange1}
+          value={email}
+        />
+        <input
+          type="text"
+          className="form-control-2"
+          onChange={handleChange2}
+          value={degree}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
---
+++ src/components/ListTodos.js
@@ -0,0 +1,80 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+import EditTodo from "./EditTodo";
+
+const ListTodos = () => {
+  const [doctors, setdoctor] = useState([]);
+
+  //delete todo function
+
+  const deleteTodo = async email => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/doctor/${email}`, {
+        method: "DELETE"
+      });
+
+      setdoctor(doctors.filter(doctor => doctor.email !== email));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/doctor");
+      const jsonData = await response.json();
+
+      setdoctor(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  //console.log(todos);
+
+  return (
+    <Fragment>
+      {" "}
+      <table class="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Degree</th>
+            <th>Email</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*<tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr> */}
+          {doctors.map(doctor => (
+            <tr>
+              <td>{doctor.degree}</td>
+              <td>{doctor.email}</td>
+            <td>
+                <EditTodo todo={doctor} />
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(doctor.email)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
